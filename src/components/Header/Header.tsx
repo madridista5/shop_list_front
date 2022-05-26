71cbb0eb5fc9e0920229252f6b3c62e71e1e0f17
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-05-26 22:13:21 +0200

    Component Header was modified.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,6 @@ import './Header.css';
 
 export const Header = () => {
     return <>
-        <div className="header">Header</div>
+        <div className="header">Find and buy your product</div>
     </>
 };
