96c4554108c2b236b6f0cc4663fea10ee6775b93
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-05-27 20:43:37 +0200

    Paths were added to component Main.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 import './Main.css';
+import {Route, Routes} from "react-router-dom";
 
 export const Main = () => {
     return <>
         <div className="main">
-            Main
+            <Routes>
+                <Route path="/" element={<h1>Strona główna</h1>}/>
+                <Route path="/add-shop" element={<h1>Dodaj sklep</h1>}/>
+                <Route path="/shop-list" element={<h1>Lista sklepów</h1>}/>
+                <Route path="/map" element={<h1>Mapa</h1>}/>
+            </Routes>
         </div>
     </>
 };
