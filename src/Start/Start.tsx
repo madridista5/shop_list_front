0087ba256e73e9428fde3c698817900596814cac
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-05-28 10:05:39 +0200

    Component Start was created.

---
+++ src/Start/Start.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Start.css';
+
+export const Start = () => (
+    <>
+        <div className="start">
+            <h1>Witamy w aplikacji "Find and buy your product"</h1>
+            <p>Nasza aplikacja pomoże Ci znaleźć produkt, którego potrzebujesz oraz porównać jego ceny w różnych sklepach.</p>
+            <p>Jeżeli jesteś właścicielem sklepu to zachęcamy Cię do dodania go do naszej aplikacji. Dzięki temu dotrzesz ze swoimi produktami do szerokiego grona osób użytkowników apliakacji "Find and buy your product" :)</p>
+        </div>
+    </>
+);
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import './Main.css';
 import {Route, Routes} from "react-router-dom";
+import {Start} from "../../Start/Start";
 
 export const Main = () => {
     return <>
         <div className="main">
             <Routes>
-                <Route path="/" element={<h1>Strona główna</h1>}/>
+                <Route path="/" element={<Start/>}/>
                 <Route path="/add-shop" element={<h1>Dodaj sklep</h1>}/>
                 <Route path="/shop-list" element={<h1>Lista sklepów</h1>}/>
                 <Route path="/map" element={<h1>Mapa</h1>}/>
