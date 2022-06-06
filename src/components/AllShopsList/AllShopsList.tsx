34aec67a39b9b945c3281094b9ee344efbf5a289
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-06-06 20:06:24 +0200

    Component MapSearchForm was added to the shops list view.

--- src/components/AllShopsList/AllShopsList.tsx
+++ src/components/AllShopsList/AllShopsList.tsx
@@ -1,11 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 import './AllShopsList.css';
 import {apiUrl} from "../../config/api";
 import { ShopEntity } from "types";
 import {Link} from "react-router-dom";
+import {MapSearchForm} from "../Map/MapSearchForm";
+import {SearchContext} from "../../contexts/search.context";
 
 export const AllShopsList = () => {
+    const {search} = useContext(SearchContext);
     const [shops, setShops] = useState<ShopEntity[]>([]);
 
     useEffect(() => {
@@ -16,23 +19,33 @@ export const AllShopsList = () => {
         })();
     }, []);
 
-    return <div className="shops-wrapper">
-        <h2>Lista sklepów</h2>
-        <div className="single-shop-wrapper">
-            {
-                shops.map(shop => (<li key={shop.id}>
-                    <p>Nazwa: {shop.name}</p>
-                    <p>Kategoria: {shop.category}</p>
-                    {shop.url && <p>Adres URL: <a href={shop.url}>{shop.url}</a></p>}
-                    {/*<p>Lista produktów:</p>*/}
-                    {/*add new component with all products*/}
-                    <p className="last-p">
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`${apiUrl}/shops/${search}`);
+            const data = await res.json();
+            setShops(data);
+        })();
+    }, [search]);
+
+    return <>
+        <MapSearchForm/>
+        <div className="shops-wrapper">
+            <h2>Lista sklepów</h2>
+            <div className="single-shop-wrapper">
+                {
+                    shops.map(shop => (<li key={shop.id}>
+                        <p>Nazwa: {shop.name}</p>
+                        <p>Kategoria: {shop.category}</p>
+                        {shop.url && <p>Adres URL: <a href={shop.url}>{shop.url}</a></p>}
+                        {/*<p>Lista produktów:</p>*/}
+                        {/*add new component with all products*/}
+                        <p className="last-p">
                             <Link to="/test" className="link">Edytuj</Link>
                             <Link to="/test" className="link">Usuń</Link>
                             <Link to="/test" className="link">Szczegóły</Link>
-                    </p>
-                </li>))
-            }
-        </div>
-    </div>;
+                        </p>
+                    </li>))
+                }
+            </div>
+        </div></>;
 };
