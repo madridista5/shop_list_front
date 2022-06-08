a28ee1bb796c952db9938fbdff9b44bc275bf2cf
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-06-08 21:31:15 +0200

    Component Btn was fixed.

--- src/components/common/Btn.tsx
+++ src/components/common/Btn.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Link} from "react-router-dom";
 
 import './Btn.css';
+import {SearchContext} from "../../contexts/search.context";
 
 interface Props {
     text: string;
@@ -9,7 +10,9 @@ interface Props {
 }
 
 export const Btn = (props: Props) => {
+    const {setSearch} = useContext(SearchContext);
+
     return (
-        <Link type="submit" className="btn" to={props.to}>{props.text}</Link>
+        <Link type="submit" className="btn" to={props.to} onClick={() => setSearch('')}>{props.text}</Link>
     );
 };
